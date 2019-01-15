621afea0fd4b1b3c76d2fb7b3de7d089cbb7da35
Sergio <sergiohidalgocaceres@gmail.com>
2019-01-15 17:11:32 -0500

    Modulización

---
+++ Angular/Clase16/appModulos/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { LoginComponent } from './login/login.component';
+import { ListadoLibrosComponent } from './listado-libros/listado-libros.component';
+import { EdicionLibrosComponent } from './edicion-libros/edicion-libros.component';
+import { ListadoAutoresComponent } from './listado-autores/listado-autores.component';
+import { EdicionAutoresComponent } from './edicion-autores/edicion-autores.component';
+import { ListadoEditorialesComponent } from './listado-editoriales/listado-editoriales.component';
+import { EdicionEditorialesComponent } from './edicion-editoriales/edicion-editoriales.component';
+import { ReducidorPipe } from './reducidor.pipe';
+import { ConfirmacionComponent } from './confirmacion/confirmacion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    LoginComponent,
+    ListadoLibrosComponent,
+    EdicionLibrosComponent,
+    ListadoAutoresComponent,
+    EdicionAutoresComponent,
+    ListadoEditorialesComponent,
+    EdicionEditorialesComponent,
+    ReducidorPipe,
+    ConfirmacionComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
