8693c3c21b6869fb67e38f83e690ab2837bcdd1a
Alonzo <educatech1gmail.com>
2019-02-12 22:17:37 -0500

    upload images

--- Node/Clase24/esqueleto/routes/usuarios.route.ts
+++ Node/Clase24/esqueleto/routes/usuarios.route.ts
@@ -3,6 +3,7 @@ import { UsuariosController } from '../api/controllers/usuarios.controller'
 import { handlerErrors } from "../handlers/errors.handler";
 import { authentication } from "../api/policies/authentication.policy";
 import { authorization } from "../api/policies/authorization.policy";
+import { handlersImages } from "../handlers/images.handler";
 
 const router = express.Router()
 const controller = new UsuariosController()
@@ -10,7 +11,7 @@ const controller = new UsuariosController()
 router.get("/",authentication, authorization("admin"), handlerErrors.cacheo(controller.listar))
 router.get("/:id", handlerErrors.cacheo(controller.detallar))
 router.post("/login", handlerErrors.cacheo(controller.login))
-router.post("/", handlerErrors.cacheo(controller.insertar))
+router.post("/", handlersImages.upload(), handlersImages.save(), handlerErrors.cacheo(controller.insertar))
 router.put("/:id", handlerErrors.cacheo(controller.modificar))
 router.delete("/:id", handlerErrors.cacheo(controller.eliminar))
 router.post("/new-access-token", handlerErrors.cacheo(controller.newAccessToken))
